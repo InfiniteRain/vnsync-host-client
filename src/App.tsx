271e704631b1bf8f23ff6aba7f03c821e8a7151a
David Lysenko <lysenkodavid@gmail.com>
2021-04-13 19:05:46 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+function render() {
+  ReactDOM.render(
+    <>
+      <h2>Hello from React!</h2>
+      <span>This is a span, just wanna test the prettier installation.</span>
+    </>,
+    document.body
+  );
+}
+
+render();
