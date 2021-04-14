0408be5af6b7bcf4dfdd888b77a60bbf5a8b4d22
David Lysenko <lysenkodavid@gmail.com>
2021-04-14 20:15:18 +0300

    Decouple some inital react code

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,16 +1,10 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom";
 
-function render() {
-  ReactDOM.render(
+export const App = (): JSX.Element => {
+  return (
     <>
       <h2>Hello from React!</h2>
       <span>This is a span, just wanna test the prettier installation.</span>
-    </>,
-    document.body
+    </>
   );
-}
-
-console.log(vnSync);
-
-render();
+};
--- src/renderer.ts
+++
@@ -1,5 +0,0 @@
-import "./index.css";
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-
-import "./App.tsx";
